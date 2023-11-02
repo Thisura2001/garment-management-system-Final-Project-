a053a0a918b89dd8cd585fa0ac1bad558071c329
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2023-11-02 20:37:17 +0530

    login success

--- src/main/java/lk/ijse/Controller/CreateAccountFormController.java
+++ src/main/java/lk/ijse/Controller/CreateAccountFormController.java
@@ -1,11 +1,18 @@
 package lk.ijse.Controller;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateAccountFormController {
-
+    public String[][] saveDetail = new String[2][6];
     @FXML
     private AnchorPane rootNode;
 
@@ -26,4 +33,49 @@ public class CreateAccountFormController {
 
     @FXML
     private JFXTextField txtusername;
+    void callArray(){
+        for (int i = 0; i < saveDetail.length; i++) {
+            int count = 0;
+            for (int j = 0; j < saveDetail[i].length; j++) {
+                if (saveDetail[i][j] != null) {
+                    count++;
+                }
+            }
+            if (count != 6) {
+                saveDetail[i][0] = txtFname.getText();
+                saveDetail[i][1] = txtLname.getText();
+                saveDetail[i][2] = txtetf.getText();
+                saveDetail[i][3] = txtnic.getText();
+                saveDetail[i][4] = txtusername.getText();
+                saveDetail[i][5] = txtpassword.getText();
+                break;
+            }
+        }
+    }
+
+    public void btnCreateAccOnAction(ActionEvent actionEvent) throws IOException {
+        callArray();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/logging_form.fxml"));
+        Parent rootNode = fxmlLoader.load();
+        LoggingFormController loginFormController = fxmlLoader.getController();
+
+        loginFormController.setValues(saveDetail);
+
+        Scene scene = new Scene(rootNode);
+        Stage stage = (Stage) this.rootNode.getScene().getWindow();
+        stage.setTitle("Log in Page");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void btnSignInOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/logging_form.fxml"));
+        Parent rootNode = fxmlLoader.load();
+
+        Scene scene = new Scene(rootNode);
+        Stage stage = (Stage) this.rootNode.getScene().getWindow();
+        stage.setTitle("Sign-Up Page");
+        stage.setScene(scene);
+    }
 }
--- src/main/java/lk/ijse/Controller/LoggingFormController.java
+++ src/main/java/lk/ijse/Controller/LoggingFormController.java
@@ -6,13 +6,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.awt.*;
 import java.io.IOException;
 
-public class LoggingFormController {
+public class LoggingFormController extends CreateAccountFormController{
 
 
     @FXML
@@ -24,6 +25,10 @@ public class LoggingFormController {
     @FXML
     private JFXTextField txtusername;
 
+    public LoggingFormController(){
+        setValues(super.saveDetail);
+    }
+
     public void btnCreateAccountOnAction(ActionEvent actionEvent) throws IOException {
         AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/CreateAccount_form.fxml"));
         Scene scene = new Scene(anchorPane);
@@ -33,6 +38,63 @@ public class LoggingFormController {
         stage.centerOnScreen();
     }
 
-    public void btnLoggingOnAction(ActionEvent actionEvent) {
+    public void btnLoggingOnAction(ActionEvent actionEvent) throws IOException {
+        String username = txtusername.getText();
+        String password = txtpassword.getText();
+
+        // Replace this logic with your actual authentication mechanism.
+        boolean isLoginSuccessful = authenticateUser(username, password);
+
+        if (isLoginSuccessful) {
+            // Login successful, you can open a new scene or perform other actions.
+            showInfoAlert("Login Successful", "Welcome, " + username + "!");
+
+            System.out.println("Navigating to the Dashboard");
+            Parent rootNode = FXMLLoader.load(getClass().getResource("/view/Dashboard_form.fxml"));
+            Scene scene = new Scene(rootNode);
+            Stage stage = (Stage) this.rootNode.getScene().getWindow();
+            stage.setTitle("Elephant Pass");
+            stage.setScene(scene);
+
+        } else {
+            // Login failed, show an error message to the user.
+            showErrorAlert("Login Failed", "Invalid username or password.");
+        }
+    }
+
+    private void showErrorAlert(String title, String massage) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(massage);
+        alert.showAndWait();
+    }
+
+    private void showInfoAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private boolean authenticateUser(String username, String password) {
+        for (int i = 0; i < saveDetail.length; i++) {
+            if (saveDetail[i][4].equals(username) && saveDetail[i][5].equals(password)) {
+                System.out.println("Password correct");
+                return true;
+            }
+        }
+        // If no matching user was found
+        System.out.println("Username or password incorrect");
+        return false;
+    }
+    void setValues(String[][] sd) {
+        this.saveDetail = sd;
+        for (int i = 0; i < saveDetail.length; i++) {
+            for (int j = 0; j < saveDetail[i].length; j++) {
+                System.out.println(saveDetail[i][j]+",");
+            }
+        }
     }
 }
